5fde077e2245d9def0e4cb8930c7ba71f9c60a6a
suku-h <sukumarapptest@gmail.com>
2016-03-01 17:13:39 +0530

    all files added

--- app/src/main/java/in/jiyofit/the_app/AppApplication.java
+++ app/src/main/java/in/jiyofit/the_app/AppApplication.java
@@ -2,20 +2,12 @@ package in.jiyofit.the_app;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.util.Base64;
-import android.util.Log;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class AppApplication extends Application {
     private static AppApplication sInstance;
-    public static final String INSTAGRAM_ACCESS_TOKEN = "2698952081.ea1b6e3.9c9e5541abd44a3da1fd7bd4d991da40";
+    public static final String INSTAGRAM_ACCESS_TOKEN = "abc";
 
     public static AppApplication getsInstance(){
         return  sInstance;
@@ -37,24 +29,8 @@ public class AppApplication extends Application {
             .setFontAttrId(R.attr.fontPath)
             .build()
             );
-        printHashKey();
+
     }
 
-
-    public void printHashKey(){
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "in.jiyofit.the_app",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("Yeppa123:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-
-            }
-        } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException ignored) {
-
-        }
-    }
 }
 
